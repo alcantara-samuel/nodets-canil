d1075b42f6c9dab5911bf6b36a6a5a7bd96a7f67
Samuel Alcantara <samuel_alcantara@outlook.com.br>
2023-08-11 20:38:22 -0300

    dist add

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -4,9 +4,14 @@ import { createMenuObject } from '../helpers/createMenuObject'
 
 export const search = (req: Request, res: Response) => {
     let query: string = req.query.q as string
+    if(!query){
+        res.redirect('/') //caso pesquisa em branco
+        return //parar a execução
+    }
     let list = Pet.getFromName(query)
     res.render('pages/page', {
         menu: createMenuObject(''),
-        list
+        list,
+        query // para manter o input o que se digitou
     })
 }
